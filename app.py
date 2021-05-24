8688f26a659d59de828d64270bf2a80a167468ac
gsaidheeraj <60067496+GSaiDheeraj@users.noreply.github.com>
2021-05-24 16:45:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ import tensorflow as tf
 import os
 from tensorflow.keras.models import load_model
 
+
+#from this import SQLAlchemy
 app=Flask(__name__,template_folder='template')
 
 
 app.config['SECRET_KEY'] = "UddA58IkCqP5nZkwEzA7YA"
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -23,23 +25,23 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = 'uploads'
 STATIC_FOLDER = 'static'
 
-#from tensorflow.keras.models import load_model
-#import keras
 
-# global graph
-# graph = tf.get_default_graph()
-model4 = tensorflow.keras.models.load_model("BrainCT1.h5")
+
+#global graph
+#graph = tf.get_default_graph()
 model = tensorflow.keras.models.load_model('model111.h5')
 model1 = tensorflow.keras.models.load_model("pneumonia.h5")
 model2 = tensorflow.keras.models.load_model("Covid_model.h5")
 model3 = tensorflow.keras.models.load_model("CovidCT.h5")
+model4 = tensorflow.keras.models.load_model("BrainCT1.h5")
 
 # Malaria
 def api(full_path):
     #with graph.as_default():
     data = keras.preprocessing.image.load_img(full_path, target_size=(50, 50, 3))
     data = np.expand_dims(data, axis=0)
-    data = data*1.0/255
+    data = data * 1.0 / 255
+        #with graph.as_default():
     predicted = model.predict(data)
     return predicted
 #pneumonia
@@ -47,30 +49,30 @@ def api1(full_path):
     #with graph.as_default():
     data = keras.preprocessing.image.load_img(full_path, target_size=(224, 224, 3))
     data = np.expand_dims(data, axis=0)
-    data = data*1.0/255
+    data = data * 1.0/ 255
     predicted = model2.predict(data)
     return predicted
+
 #Covid-19
 def api111(full_path):
     #with graph.as_default():
     data = keras.preprocessing.image.load_img(full_path, target_size=(224, 224, 3))
     data = np.expand_dims(data, axis=0)
-    data = data*1.0/255
+    data = data * 1.0/ 255
     predicted = model2.predict(data)
     return predicted
 def api1111(full_path):
     #with graph.as_default():
     data = keras.preprocessing.image.load_img(full_path, target_size=(224, 224, 3))
     data = np.expand_dims(data, axis=0)
-    data = data*1.0/255
+    data = data * 1.0/ 255
     predicted = model3.predict(data)
     return predicted
-#brain
 def api2(full_path):
     #with graph.as_default():
     data = keras.preprocessing.image.load_img(full_path, target_size=(224, 224, 3))
     data = np.expand_dims(data, axis=0)
-    data = data*1.0/255
+    data = data * 1.0/ 255
     predicted = model4.predict(data)
     return predicted
 
@@ -85,9 +87,11 @@ def upload_file():
             file = request.files['image']
             full_name = os.path.join(UPLOAD_FOLDER, file.filename)
             file.save(full_name)
+
             indices = {0: 'PARASITIC', 1: 'Uninfected'}
             result = api(full_name)
             print(result)
+
             predicted_class = np.asscalar(np.argmax(result, axis=1))
             accuracy = round(result[0][predicted_class] * 100, 2)
             label = indices[predicted_class]
@@ -139,10 +143,8 @@ def upload111_file():
             file = request.files['image']
             full_name = os.path.join(UPLOAD_FOLDER, file.filename)
             file.save(full_name)
-
             indices = {1: 'Healthy', 0: 'Corona-Infected'}
             result = api111(full_name)
-
             predicted_class = np.asscalar(np.argmax(result, axis=1))
             accuracy = round(result[0][predicted_class] * 100, 2)
             label = indices[predicted_class]
@@ -150,6 +152,7 @@ def upload111_file():
                 prediction = "Please, Check with the Doctor."
             else:
                 prediction = "Result is accurate"
+
             return render_template('coronapredict.html', image_file_name = file.filename, label = label, accuracy = accuracy, prediction=prediction)
         except:
             flash("Please select the X-ray image first !!", "danger")
@@ -177,9 +180,9 @@ def upload1111_file():
 
             return render_template('coronapredict1.html', image_file_name = file.filename, label = label, accuracy = accuracy, prediction=prediction)
         except:
-            flash("Please select the X-ray image first !!", "danger")
-            return redirect(url_for("covid_19"))
-
+            flash("Please select the CT-Scan image first !!", "danger")
+            return redirect(url_for("covidct_19"))
+	
 @app.route('/upload2', methods=['POST', 'GET'])
 def upload2_file():
     #with graph.as_default():
@@ -190,8 +193,8 @@ def upload2_file():
             file = request.files['image']
             full_name = os.path.join(UPLOAD_FOLDER, file.filename)
             file.save(full_name)
-            indices = {1: 'Healthy', 0: 'Brain Tumor'}
-            result = api2(full_name)
+            indices = {1: 'Healthy', 0: 'Brain-Tumor'}
+            result = api1111(full_name)
             predicted_class = np.asscalar(np.argmax(result, axis=1))
             accuracy = round(result[0][predicted_class] * 100, 2)
             label = indices[predicted_class]
@@ -202,9 +205,9 @@ def upload2_file():
 
             return render_template('brainpredict.html', image_file_name = file.filename, label = label, accuracy = accuracy, prediction=prediction)
         except:
-            flash("Please select the X-ray image first !!", "danger")
+            flash("Please select the CT-Scan image first !!", "danger")
             return redirect(url_for("Brain"))
-
+	
 @app.route('/uploads/<filename>')
 def send_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
@@ -220,11 +223,12 @@ def about():
     return render_template("about.html")
 
 @app.route("/Brain")
-def Brain():
+def about():
     return render_template("brain.html")
 
 @app.route("/covid_19")
 def covid_19():
+    # if form.validate_on_submit():
     return render_template("corona.html")
 
 @app.route("/covidct_19")
